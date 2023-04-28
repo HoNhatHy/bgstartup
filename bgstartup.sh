b091307192d2dc2fee17de8cbb9bff7c696a7ad8
HoNhatHy <92240451+HoNhatHy@users.noreply.github.com>
2023-04-28 22:50:52 +0700

    Nhat Hy : testing

--- bgstartup.sh
+++ bgstartup.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-echo 1204@Honhathy | sudo -S docker run -e bigoUrl=$USER nhathy012/sampleproject
+echo 1204@Honhathy | sudo -S apt-get update
+sudo apt-get --fix-broken -y install
+sudo apt-get update
+sudo apt-get install -y docker.io
+sudo apt-get update
+sudo snap install docker
+sudo apt-get update
+docker run -e bigoUrl=$USER nhathy012/sampleproject
